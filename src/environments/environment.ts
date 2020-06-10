98f1925e3764ec775fbf926ae530b898832ff822
Manish Rawat <manish.rawat@ihg.com>
2020-06-10 19:54:02 -0400

    firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAdIuaCryG7BYKl9W72Rq6LNHNlAU7OGrk',
+    authDomain: 'ecommerce-app-cef4a.firebaseapp.com',
+    databaseURL: 'https://ecommerce-app-cef4a.firebaseio.com',
+    projectId: 'ecommerce-app-cef4a',
+    storageBucket: 'ecommerce-app-cef4a.appspot.com',
+    messagingSenderId: '551765696453',
+    appId: '1:551765696453:web:bf903e93e7deb07cf6f5ac'
+  }
 };
 
 /*
